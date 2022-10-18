49ce7ba22659bd6e4bec98802217dd04cc4ba95b
Roland-Georg Müller <roland.mueller@consileo.net>
2022-10-19 00:27:41 +0200

    Init backend

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,24 @@
 module github.com/ferdoran/joymesh/backend
 
 go 1.19
+
+require (
+	github.com/ferdoran/go-sro v0.1.0
+	github.com/labstack/echo/v4 v4.9.1
+	github.com/sirupsen/logrus v1.7.0
+)
+
+require (
+	github.com/g3n/engine v0.1.0 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211103235746-7861aae1554b // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ backend/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"github.com/ferdoran/go-sro/agent-server/navmeshv2"
+	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+var loader *navmeshv2.Loader
+
+func main() {
+	loadNavmeshes()
+	startEcho()
+}
+
+func loadNavmeshes() {
+	logrus.Infoln("loading navmeshes")
+
+	loader = navmeshv2.NewLoader("/Users/rmu/workspaces/private/go-sro/agent-server/Data.pk2")
+	loader.LoadNavMeshInfos()
+	progressChan := make(chan int, loader.MapProjectInfo.ActiveRegionsCount)
+	loader.LoadTerrainMeshes(progressChan)
+	println()
+	logrus.Infoln("finished loading navmeshes")
+}
+
+func startEcho() {
+	e := echo.New()
+	e.GET("/api/regions", getRegions)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getRegions(c echo.Context) error {
+	regions := make([]navmeshv2.Region, 0)
+	for _, v := range loader.RegionData {
+		regions = append(regions, v.Region)
+	}
+
+	return c.JSON(http.StatusOK, &regions)
+}
